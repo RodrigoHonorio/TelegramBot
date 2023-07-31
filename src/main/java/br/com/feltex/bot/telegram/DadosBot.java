2fbb8361d2e221903e0aa1e5daf40ed8cea08c2d
rodrigo <rodrigo_mhonorio@hotmail.com>
2023-07-31 18:23:23 +0100

    First commit

---
+++ src/main/java/br/com/feltex/bot/telegram/DadosBot.java
@@ -0,0 +1,10 @@
+package br.com.feltex.bot.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class DadosBot {
+
+    public static final String BOT_TOKEN ="6530894929:AAFr8YADTx8tycKNAO6sj4LhZAsqsbUYeWM";
+
+    public static final String BOT_USER_NAME ="@RodrigoHonorioBot" ;
+}
